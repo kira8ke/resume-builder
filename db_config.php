bc7a2281eb0597a7c6ffd2fd15e44ff1168cb281
Gloria <wanyagagloria43@email.com>
2025-04-22 21:41:16 +0300

    changing conn to pdo

--- db_config.php
+++ db_config.php
@@ -1,12 +1,12 @@
 <?php
 $host = "localhost";
 $db_name = "resume_builder";
-$username = "root"; // Change if needed
-$password = ""; // Change if needed
+$username = "root";
+$password = "";
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
